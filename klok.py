06d559ec754542ec06c91adfecf11985db0ffa1c
michielbrink <michielpa@gmail.com>
2014-08-30 19:03:33 +0200

    24pm

--- klok.py
+++ klok.py
@@ -3,9 +3,9 @@ import time
 
 timeold = 0
 while 1:
-	timenew = [time.strftime('%I')[:1] ,time.strftime('%I')[1:] ,':' ,time.strftime('%M')[:1] ,time.strftime('%M')[1:] ,' ' ,time.strftime('%p')[:1] ,time.strftime('%p')[1:]]
+	timenew = [time.strftime('%H') ,':' ,time.strftime('%M')]
 	timenew = ''.join(map(str,timenew))
 	if timenew != timeold:
-		os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=     '" + timenew)
+		os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=      '" + timenew)
 		timeold = timenew
 	time.sleep(4)
