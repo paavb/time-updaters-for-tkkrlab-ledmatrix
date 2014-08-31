fe15ff1512eeec6ab204f41be15c8114f738f5dd
Stijn van de Ridder <stijnvanderidder@gmail.com>
2014-08-31 12:55:19 +0200

    as you requested

---
+++ improvedklok.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import time
+
+
+while(1):
+	time.sleep(1);
+	output = subprocess.check_output(["date", "+%T"]);
+	os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text= '" + output)
