6c59d427cdcc4244c8ebcdcb87035357df3da5dc
Robert <201292@live.nl>
2014-09-02 19:04:57 +0200

    duality, added some text

---
+++ improvedklok-Duality-ikbengeenboots.py
@@ -0,0 +1,7 @@
+import os
+import subprocess
+import time
+while(1):
+	time.sleep(1);
+	output = subprocess.check_output(["date", "+%T"]);
+	os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=<B0075> Ik ben geen boots.      '" + output)
