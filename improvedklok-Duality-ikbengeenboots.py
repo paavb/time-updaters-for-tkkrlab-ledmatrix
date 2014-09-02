85e566e6a93b17b257ea9597f24ce253f70ba229
Paola <paolavanbavel@gmail.com>
2014-09-02 19:06:05 +0200

    Update improvedklok-Duality-ikbengeenboots.py

--- improvedklok-Duality-ikbengeenboots.py
+++ improvedklok-Duality-ikbengeenboots.py
@@ -4,4 +4,4 @@ import time
 while(1):
 	time.sleep(1);
 	output = subprocess.check_output(["date", "+%T"]);
-	os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=<B0075> Ik ben geen boots.      '" + output)
+	os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=HAXOR" + output)
